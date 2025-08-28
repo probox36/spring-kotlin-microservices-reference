7c0e21cf6fd4a76e618651d61477cccc25cbdee8
probox36 <probox36@gmail.com>
2025-08-29 00:05:32 +0600

    Added kafka topic config

---
+++ common-module/src/main/kotlin/config/kafka/KafkaTopicConfig.kt
@@ -0,0 +1,35 @@
+package com.buoyancy.common.config.kafka
+
+import com.buoyancy.common.model.enums.TopicNames
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+
+@Configuration
+class KafkaTopicConfig {
+
+    @Bean
+    fun ordersTopic(): NewTopic {
+        return TopicBuilder.name(TopicNames.ORDER)
+            .partitions(3)
+            .replicas(1)
+            .build()
+    }
+
+    @Bean
+    fun paymentsTopic(): NewTopic {
+        return TopicBuilder.name(TopicNames.PAYMENT)
+            .partitions(3)
+            .replicas(1)
+            .build()
+    }
+
+    @Bean
+    fun suborderTopic(): NewTopic {
+        return TopicBuilder.name(TopicNames.SUBORDER)
+            .partitions(3)
+            .replicas(1)
+            .build()
+    }
+}
